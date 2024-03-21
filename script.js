6d140097bb469230feddf1effcfbeb3cd0fb854a
Laura_Bal <baltrukone@gmail.com>
2024-03-21 22:06:55 +0200

    Template strings 2

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
+function print (name, age, profession, gender) {
 
-let name = 'Tom';
-console.log (`Hello ${name}!`);
-console.log ('Hello '+ name);
+	document.write (`
+Name ${name} <br>
+Age: ${age}<br>
+Profession: ${profession} <br>
+Gender: ${gender}
+`)
 
-function print(name, age, profession, gender){
-	document.write("Name: " + name);
-	document.write('<br>');
-	document.write("Age: " + age);
-	document.write('<br>');
-	document.write("Profession: " + profession);
-	document.write('<br>');
-	document.write("Gender: " + gender);
+	console.log (`
+			Name ${name}
+			Age: ${age}
+		  Profession: ${profession} 
+		  Gender: ${gender}	
+		`
+	)
+	// document.write("Name: " + name);
+	//
+	// document.write("Age: " + age);
+	//
+	// document.write("Profession: " + profession);
+	//
+	// document.write("Gender: " + gender);
 }
 
-print("Tom", 30, "Programmer", "male");
+print ("Tom", 30, "Programmer", "male");
