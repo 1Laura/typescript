0173369fbe2a7269df92fc33af594bd9127afa96
Laura_Bal <baltrukone@gmail.com>
2024-03-22 16:27:44 +0200

    Data Structure: Maps

--- script.js
+++ script.js
@@ -3,6 +3,33 @@ let kate = ['Kate', '30', 'programmer', 'playing football'];
 let tom = ['Tom', '29', 'runner', 'playing games'];
 let julie = ['Julie', '23', 'tester', 'playing hockey'];
 
-// let person=[...kate, 'Marc', '27', 'engineer', 'computing', ...julie];
-let person=['Marc', '27', ...kate, 'engineer', 'computing', julie];
-console.log (person);
+let person = new Map([
+		[kate],
+		[new Date(), 'today']
+]);
+
+console.log (person)
+
+
+
+
+// let person = new Map();
+// person.set(function print(){
+// 	console.log ("hey");
+// }, 'Marc');
+
+// person.set('name1', 'Marc1');
+// person.set('name2', 'Marc2');
+// person.set('name3', 'Marc3');
+// person.set('name4', 'Marc4');
+// person.set('name5', 'Marc5');
+
+// person.forEach(function (person) {
+// 	console.log (person)
+// })
+// console.log (person.values());
+// console.log (person.keys());
+// console.log (person.get('name3'));
+// console.log (person.has('name3'));
+// console.log (person.has('name7'));
+
